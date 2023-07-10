0a5f9f19b213fbf1360d750285e709fe7d0e43ba
00-berni <bernardo.vettori@outlook.it>
2023-07-10 12:45:24 +0200

    - Adapted script to new version of func - Updated plot routine

--- script.py
+++ script.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
-from visibility.stuff import get_data
 from visibility.Time.Tclasses import Date 
 from visibility.sky import *
 
 def import_data(filename, sel: int | slice = slice(None), delimiter: str =',', sep: str = ':'):
+    from visibility.stuff import get_data
     names, ras, decs, muas, muds = get_data(filename,delimiter=delimiter)
     
     prmts = []
@@ -26,25 +26,22 @@ def import_data(filename, sel: int | slice = slice(None), delimiter: str =',', s
 
 
 if __name__ == '__main__':
-
-    Npnt = 200
+    Npnt = 1000
 
     lat = 43.93039980730644
     lon = 10.907521220934262 
     h = 65
-
     obs = GeoPos(lon,lat,h)
 
     name, ra, dec, prmt = import_data('targets.csv', sel=4)
-
     obj = StarObj(name,[ra,dec],prmt)
     obj.obj_info()
 
     # ---
 
     print()
-    date = Date([1898,6,23],[20,28,0])
-    print('Observation on '+date.str_date())
+    date = Date([2020,10,11],[14,15,0])
+    print('Observation on ' + date.print_date())
     alt, dayrange = trajectory(date,obs,obj,Npnt) 
     m = tran_ris_set(date,obs,obj,True)
     event = Date(date.date,m)
@@ -52,29 +49,26 @@ if __name__ == '__main__':
 
 
     hour = date.time.hour() 
-
-    # dayrange = np.array([calendar_date(day/24+JDdate).daydec() for day in np.linspace(0,24,numpoint)])
-
-    N = int(max(dayrange))+1 if hour <= (int(hour)+0.5) else int(max(dayrange))+2
-    ticks = np.arange(int(min(dayrange)), N,1)
+    N = 25 if hour <= (int(hour)+0.5) else 26
+    ticks = Date(date.date, (np.arange(0,N,1)+int(date.time.hour()))*3600,calendar=date.calendar,epoch=date.epoch)
 
     plt.figure(figsize=[12,8])
     ax = plt.axes()
     ax.set_facecolor('black')
-    plt.title(obj.name + ' on ' + date.str_date() + f" at\nlat: {obs.lat.str_angle('deg')}, lon: {obs.lon.str_angle('deg')}, h: {obs.h} m a.s.l.")
+    plt.title(obj.name + ' on ' + date.print_date() + f" at\nlat: {obs.lat.print_angle('deg')}, lon: {obs.lon.print_angle('deg')}, h: {obs.h} m a.s.l.")
     
     plt.plot(dayrange,alt.deg,label='trajectory')
     plt.plot(dayrange[0],alt.deg[0],'ow',label='start')
     if type(m.val) != np.ndarray:
-        plt.plot(m.hour(),ealt.deg,'vg',label='transit')
+        plt.plot(event.jd,ealt.deg,'vg',label='transit')
     else:
-        plt.plot(m.hour()[0],ealt.deg[0],'vg',label='transit')
-        plt.plot(m.hour()[1],ealt.deg[1],'vy',label='rising')
-        plt.plot(m.hour()[2],ealt.deg[2],'vr',label='setting')
+        plt.plot(event.jd[0],ealt.deg[0],'vg',label='transit')
+        plt.plot(event.jd[1],ealt.deg[1],'vy',label='rising')
+        plt.plot(event.jd[2],ealt.deg[2],'vr',label='setting')
 
 
     plt.axhline(0,xmin=0,xmax=1,linestyle='dashed',alpha=0.5,color='white',label='horizon')
-    plt.xticks(ticks,np.where(ticks < 24, ticks, ticks-24))
+    plt.xticks(ticks.jd,np.round(ticks.time.hour(),0).astype(int))
     plt.xlabel('UT [h]')
     plt.ylabel('alt$_0$ [deg]')
     plt.grid(axis='x',linestyle='dotted',color='gray',alpha=0.7)
@@ -86,7 +80,7 @@ if __name__ == '__main__':
 
     print()
     date = Date([2023,6,26],[11,5,20])
-    print('Observation on '+date.str_date())
+    print('Observation on '+date.print_date())
     alt, dayrange = trajectory(date,obs,obj,Npnt)
     m = tran_ris_set(date,obs,obj,True)
     event = Date(date.date,m)
@@ -95,27 +89,26 @@ if __name__ == '__main__':
 
     hour = date.time.hour() 
 
-    # dayrange = np.array([calendar_date(day/24+JDdate).daydec() for day in np.linspace(0,24,numpoint)])
-
-    N = int(max(dayrange))+1 if hour <= (int(hour)+0.5) else int(max(dayrange))+2
-    ticks = np.arange(int(min(dayrange)), N,1)
+    N = 25 if hour <= (int(hour)+0.5) else 26
+    ticks = Date(date.date, (np.arange(0,N,1)+int(date.time.hour()))*3600,calendar=date.calendar,epoch=date.epoch)
 
     plt.figure(figsize=[12,8])
     ax = plt.axes()
     ax.set_facecolor('black')
-    plt.title(obj.name + ' on ' + date.str_date() + f" at\nlat: {obs.lat.str_angle('deg')}, lon: {obs.lon.str_angle('deg')}, h: {obs.h} m a.s.l.")
+    plt.title(obj.name + ' on ' + date.print_date() + f" at\nlat: {obs.lat.print_angle('deg')}, lon: {obs.lon.print_angle('deg')}, h: {obs.h} m a.s.l.")
     
     plt.plot(dayrange,alt.deg,label='trajectory')
     plt.plot(dayrange[0],alt.deg[0],'ow',label='start')
     if type(m.val) != np.ndarray:
-        plt.plot(m.hour(),ealt.deg,'vg',label='transit')
+        plt.plot(event.jd,ealt.deg,'vg',label='transit')
     else:
-        plt.plot(m.hour()[0],ealt.deg[0],'vg',label='transit')
-        plt.plot(m.hour()[1],ealt.deg[1],'vy',label='rising')
-        plt.plot(m.hour()[2],ealt.deg[2],'vr',label='setting')
+        plt.plot(event.jd[0],ealt.deg[0],'vg',label='transit')
+        plt.plot(event.jd[1],ealt.deg[1],'vy',label='rising')
+        plt.plot(event.jd[2],ealt.deg[2],'vr',label='setting')
+
 
     plt.axhline(0,xmin=0,xmax=1,linestyle='dashed',alpha=0.5,color='white',label='horizon')
-    plt.xticks(ticks,np.where(ticks < 24, ticks, ticks-24))
+    plt.xticks(ticks.jd,np.round(ticks.time.hour(),0).astype(int))
     plt.xlabel('UT [h]')
     plt.ylabel('alt$_0$ [deg]')
     plt.grid(axis='x',linestyle='dotted',color='gray',alpha=0.7)
