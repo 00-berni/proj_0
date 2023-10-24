669d3ee1294fb1b4d82b18bf368eea0d039c832e
00-berni <bernardo.vettori@outlook.it>
2023-10-25 00:01:48 +0200

    Add version and author

--- visibility/__init__.py
+++ visibility/__init__.py
@@ -1,3 +1,6 @@
 from .Angles import *
 from .Time.Tclasses import *
 from .coor import Equatorial, Ecliptical, AltAz, GeoPos
+
+__version__ = '1.0.0'
+__author__ = 'Bernardo Vettori => github: 00-berni'
