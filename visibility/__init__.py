4d6cecb2da7321f71e7ad78ff64af9437470a775
00-berni <bernardo.vettori@outlook.it>
2023-10-25 12:19:30 +0200

    Problems with version and authors

--- visibility/__init__.py
+++ visibility/__init__.py
@@ -1,6 +1,3 @@
-from .Angles import *
-from .Time.Tclasses import *
-from .coor import Equatorial, Ecliptical, AltAz, GeoPos
 
-__version__ = '1.0.0'
-__author__ = 'Bernardo Vettori => github: 00-berni'
+# __version__ = '1.0.0'
+# __author__ = 'Bernardo Vettori => github: 00-berni'
