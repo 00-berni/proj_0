9cf62a9bdc36231ece8644733159593ba4d06d79
00-berni <bernardo.vettori@outlook.it>
2023-10-25 16:52:16 +0200

    Update

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import numpy as np
-from visibility.stuff import import_data
-
+from visibility.sky import initialize_data, visibility
 
 if __name__ == '__main__':
-    file_data = 'targets.csv'
-    data = import_data(file_data)
+    data_file = 'targets.csv'
+    sel = slice(4,None)
+    targets, observatories, dates = initialize_data(data_file,sel=sel)    
+
+
+    for (obj,obs,date) in zip(targets,observatories,dates):
+        _ = visibility(date,obj,obs)
