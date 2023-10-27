3de7194c81a969c150feeef7837e865ae96e34a3
00-berni <bernardo.vettori@outlook.it>
2023-10-28 01:25:31 +0200

    Update

--- script.py
+++ script.py
@@ -3,10 +3,12 @@ from visibility.sky import initialize_data, visibility
 
 if __name__ == '__main__':
     data_file = 'targets.csv'
-    st, end = 0,1
+    st, end = 1,3
     sel = slice(st,end)
+    window = True
+    # window = True
     targets, observatories, dates = initialize_data(data_file,sel=sel)    
 
 
     for (obj,obs,date) in zip(targets,observatories,dates):
-        _ = visibility(date,obj,obs)
+        _ = visibility(date,obj,obs,win=window)
